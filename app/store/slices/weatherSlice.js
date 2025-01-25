d010ae6a48ad964d5b53370ea35e81de06518f50
meccacodes <hi@windfallyarn.com>
2025-01-25 11:28:46 -0800

    Basic app working as per insturctions, needs styling.

---
+++ app/store/slices/weatherSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_KEY = "3f88d5f8bbfa8eb651ea5f78d37f3578";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
+
+export const fetchWeather = createAsyncThunk(
+  "weather/fetchWeather",
+  async (city) => {
+    const response = await axios.get(
+      `${BASE_URL}?q=${city}&units=imperial&appid=${API_KEY}`
+    );
+    return response.data;
+  }
+);
+
+const weatherSlice = createSlice({
+  name: "weather",
+  initialState: {
+    loading: false,
+    data: {},
+    error: "",
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWeather.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchWeather.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+        state.error = "";
+      })
+      .addCase(fetchWeather.rejected, (state, action) => {
+        state.loading = false;
+        state.data = {};
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default weatherSlice.reducer;
